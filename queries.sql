354ac16b7e86cf6921a81298f97f47869edfdd58
Andrew <awoodcoc@indiana.edu>
2023-11-02 21:16:10 -0500

    finish task 2 other than the dropdown menus

--- queries.sql
+++ queries.sql
@@ -1,6 +1,16 @@
 --Part 1
 
+CREATE TABLE job (
+    id INT,
+    employer VARCHAR(255),
+    name VARCHAR(255),
+    skills VARCHAR(255)
+);
+
 --Part 2
+
+SELECT * FROM job
+WHERE job.employer = "St. Louis";
 
 --Part 3
 
